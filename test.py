add9274fbdf43b07648f4e8c154e3e3b19ba4f99
Sampo Pelto <pelto.sampo@gmail.com>
2023-01-11 15:18:06 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,35 @@
+import json
+import urllib.request
+import dateutil  
+from dateutil import parser
+
+print()
+
+# 4106 Tupsula -> Hervanta
+# 4097 Tupsula -> Keskusta
+stopIDList = [4097, 4106]
+
+for stopID in stopIDList:
+    
+    # Fetch data about stopping busses
+    with urllib.request.urlopen("https://data.itsfactory.fi/journeys/api/1/stop-monitoring?stops=" + str(stopID)) as url:
+        data = json.load(url)
+        # print(data)
+
+    # Fetch data about single stop for a name
+    with urllib.request.urlopen("http://data.itsfactory.fi/journeys/api/1/stop-points/" + str(stopID)) as url:
+        stopData = json.load(url)
+
+    print("Stop: " + str(stopID) + " - " + str(stopData['body'][0]['name']))
+
+    for stop in data['body'][str(stopID)]:
+
+        # Sometimes there is no aimedArrivalTime key
+        if 'aimedArrivalTime' in stop['call']:
+            aimedArrivalTime = parser.parse(stop['call']['aimedArrivalTime'])
+        else:
+            aimedArrivalTime = parser.parse(stop['call']['expectedDepartureTime'])
+
+        print("Bus: " + stop['lineRef']+ ", Time Arrival: " + aimedArrivalTime.strftime("%H:%M"))
+    
+    print()
